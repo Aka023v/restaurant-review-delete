c100272638b87b87ebdbd3294386858066fabad3
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-11-17 18:39:59 +0900

    作成

--- src/delete.php
+++ src/delete.php
@@ -1,14 +1,2 @@
 <?php
-$id = filter_input(INPUT_GET, 'id');
-
-$dbh = new PDO(
-    'mysql:dbname=restaurant_review;host=mysql;charset=utf8',
-    'root',
-    'password'
-);
-$query = "DELETE FROM review where id = '$id'";
-$dbh->query($query);
-
-header('Location: ./index.php');
-exit;
-?>
+echo "一覧表示機能を作成しよう！";
